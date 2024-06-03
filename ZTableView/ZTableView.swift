fd57546c9a383b898f54858d2e44c03e4a5f6b24
张旻可 <zhangminke@51yund.com>
2024-06-03 17:26:54 +0800

    compile success

---
+++ ZTableView/ZTableView.swift
@@ -0,0 +1,50 @@
+//
+//  ZTableView.swift
+//  ZTableView
+//
+//  Created by 赖依娴 on 2024/5/19.
+//
+
+import UIKit
+
+
+
+open class ZTableView: UITableView {
+    public enum ZTableViewExpansionType {
+        case multiple
+        case single
+    }
+    open var expansionAnimation: UITableView.RowAnimation = .top
+    open var expansionType: ZTableViewExpansionType = .multiple
+    open var datas: [ZTableViewNodeProtocol] = [] {
+        didSet {
+            controller.datas = datas
+        }
+    }
+    open var autoSolveDataSource: Bool = true
+    open weak var zDelegate: ZTableViewDelegate? {
+        didSet {
+            controller.delegate = self.zDelegate
+            self.delegate = controller
+            self.dataSource = controller
+            self.dragDelegate = controller
+            self.dropDelegate = controller
+        }
+    }
+    
+    internal var controller: ZTableViewController = ZTableViewController()
+    override public init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+        controller.tableView = self
+//        self.register(ZTableViewCell.self, forCellReuseIdentifier: zCellID)
+    }
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        controller.tableView = self
+//        self.register(ZTableViewCell.self, forCellReuseIdentifier: zCellID)
+    }
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+        controller.tableView = self
+    }
+}
--- ZUIKitDemo/ViewController.swift
+++ ZUIKitDemo/ViewController.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
+import ZUIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            // Do any additional setup after loading the view.
+        self.view.translatesAutoresizingMaskIntoConstraints = false
+        let tableView = ZTableView(frame: CGRectZero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(tableView)
+        let topC = NSLayoutConstraint(item: tableView, attribute: .top, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .top, multiplier: 1, constant: 0)
+        let bottomC = NSLayoutConstraint(item: tableView, attribute: .bottom, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .bottom, multiplier: 1, constant: 0)
+        let leftC = NSLayoutConstraint(item: tableView, attribute: .leading, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .leading, multiplier: 1, constant: 0)
+        let rightC = NSLayoutConstraint(item: tableView, attribute: .trailing, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .trailing, multiplier: 1, constant: 0)
+        self.view.addConstraints([topC, bottomC, leftC, rightC])
     }
-
-
+    
+    
 }
 
